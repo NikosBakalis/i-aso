3bff36e76714046bf9dae39951670dca594a1c36
NikosBakalis <bakalis.nikolas@gmail.com>
2020-04-09 22:27:08 +0300

    File management!

--- Java/src/test.java
+++ Java/src/test.java
@@ -12,7 +12,7 @@ public class test {
 
     void createConnection(){
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/iaso_hospital_db_v06", "root", "password");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://iaso_db:3306/iaso_hospital_db_v06", "root", "password");
             System.out.println("LELLELELELELEL");
             System.out.println("Database Connection Success!");
         } catch (SQLException ex) {
