7d22e854ab90f204bbfebcadfe57561c65d9c8b5
NikosBakalis <bakalis.nikolas@gmail.com>
2020-04-10 00:16:32 +0300

    File management!

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
+    public static void main(String[] args) {
+        createConnection();
+    }
+
+    static void createConnection() {
+        System.out.println("lelelel");
     }
 }
