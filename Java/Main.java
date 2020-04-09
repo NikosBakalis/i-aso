5a289fefaffe1ccedede45584b28082e73141fd4
NikosBakalis <bakalis.nikolas@gmail.com>
2020-04-10 00:43:26 +0300

    Connection established successfully with the database server.

--- Java/Main.java
+++ Java/Main.java
@@ -1,9 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        createConnection();
+        openConnection();
     }
 
-    static void createConnection() {
-        System.out.println("lelelel");
+    static void openConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/iaso_hospital_db_v06", "root", "password");
+            System.out.println("Connection established successfully with the database server.");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
