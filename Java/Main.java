2e3741a477c4a63c0552711cccae9301f088d531
NikosBakalis <bakalis.nikolas@gmail.com>
2020-04-10 22:46:56 +0300

    Connection problem solved!

--- Java/Main.java
+++ Java/Main.java
@@ -6,19 +6,10 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        openConnection();
+//        openConnection();
         ICRUDImpl obj = new ICRUDImpl();
         obj.getHospital("123456");
     }
 
-    static void openConnection(){
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/iaso_hospital_db_v06", "root", "password");
-            System.out.println("Connection established successfully with the database server.");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            e.printStackTrace();
-        }
-    }
+
 }
--- Java/database/ICRUDImpl.java
+++ Java/database/ICRUDImpl.java
@@ -1,7 +1,5 @@
 package database;
 
-import com.mysql.cj.jdbc.JdbcConnection;
-
 import model.Doctor;
 import model.Hospital;
 import model.User;
@@ -11,7 +9,8 @@ import java.sql.*;
 
 public class ICRUDImpl implements ICRUD {
 
-    private JdbcConnection connection;
+    private static Connection connection;
+
 
     public User getUser(String username) {
         return null;
@@ -23,24 +22,25 @@ public class ICRUDImpl implements ICRUD {
 
     public Hospital getHospital(String afm) {
         try {
+            openConnection();
             String query = "SELECT * FROM hospital WHERE hospital.hospital_afm = '" + afm + "'";
 
             ResultSet resultSet;
             Hospital hospital;
-            try (PreparedStatement preparedStatement = this.getConnection().prepareStatement(query)) {
+            try (PreparedStatement preparedStatement = getConnection().prepareStatement(query)) {
                 resultSet = preparedStatement.executeQuery();
                 hospital = null;
                 if(resultSet.next()) {
                     hospital = new Hospital();
-                    hospital.getAfm(resultSet.getString("hospital_afm"));
-                    hospital.getName(resultSet.getString("hospital_name"));
-                    hospital.getFirst_street_name(resultSet.getString("first_street_name"));
-                    hospital.getFirst_street_number(resultSet.getString("first_street_number"));
-                    hospital.getPrimary_phone_number(resultSet.getString("primary_phone_number"));
-                    hospital.getEmergency_phone_number(resultSet.getString("emergency_phone_number"));
-                    hospital.getEmail_address(resultSet.getString("email_address"));
-                    hospital.getSecond_street_name(resultSet.getString("second_street_name"));
-                    hospital.getSecond_street_number(resultSet.getString("second_street_number"));
+                    hospital.setAfm(resultSet.getString("hospital_afm"));
+                    hospital.setName(resultSet.getString("hospital_name"));
+                    hospital.setFirst_street_name(resultSet.getString("first_street_name"));
+                    hospital.setFirst_street_number(resultSet.getInt("first_street_number"));
+                    hospital.setPrimary_phone_number(resultSet.getInt("primary_phone_number"));
+                    hospital.setEmergency_phone_number(resultSet.getInt("emergency_phone_number"));
+                    hospital.setEmail_address(resultSet.getString("email_address"));
+                    hospital.setSecond_street_name(resultSet.getString("second_street_name"));
+                    hospital.setSecond_street_number(resultSet.getInt("second_street_number"));
                 }
             }
             resultSet.close();
@@ -50,12 +50,22 @@ public class ICRUDImpl implements ICRUD {
         }
     }
 
-    public Connection getConnection() {
+    void openConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            setConnection(DriverManager.getConnection("jdbc:mysql://localhost:3306/iaso_hospital_db_v06", "root", "password"));
+            System.out.println("Connection established successfully with the database server.");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
         return connection;
     }
 
     public void setConnection(Connection connection) {
-        this.connection = (JdbcConnection) connection;
+        ICRUDImpl.connection = connection;
     }
-
 }
