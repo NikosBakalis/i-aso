0d9757a91873ef80a860fc69966a8a312d040a84
NikosBakalis <bakalis.nikolas@gmail.com>
2020-04-10 23:03:58 +0300

    Clean up code!

--- Java/Main.java
+++ Java/Main.java
@@ -1,9 +1,5 @@
 import database.ICRUDImpl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
         ICRUDImpl obj = new ICRUDImpl();
