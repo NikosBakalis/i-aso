4caaeace386960f412e46e907938c7f1e166b274
NikosBakalis <bakalis.nikolas@gmail.com>
2020-04-12 02:26:38 +0300

    CRITICAL FIXES TO ALL SCREENS!!!

--- Java/ui/DoctorPatientFile.java
+++ Java/ui/DoctorPatientFile.java
@@ -1,4 +1,25 @@
 package ui;
 
-public class DoctorPatientFile {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DoctorPatientFile extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("doctor_patient_file.fxml"));
+        Scene scene = new Scene(parent, 550, 250);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
