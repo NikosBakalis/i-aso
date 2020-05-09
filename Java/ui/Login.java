eed99e5228ce3ef37b895dbc1d7e9877f7313bb4
NikosBakalis <bakalis.nikolas@gmail.com>
2020-05-09 20:08:20 +0300

    Close window action added!

--- Java/ui/Login.java
+++ Java/ui/Login.java
@@ -2,11 +2,17 @@ package ui;
 
 import database.ICRUDImpl;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.User;
+
+import java.io.IOException;
 
 public class Login {
 
@@ -21,7 +27,7 @@ public class Login {
     ICRUDImpl iCRUDImpl = new ICRUDImpl();
     User user = new User();
 
-    public void clickLoginButton(ActionEvent actionEvent) {
+    public void clickLoginButton(ActionEvent actionEvent) throws IOException {
         if(!usernameTextId.getText().trim().isEmpty() && !passwordTextId.getText().trim().isEmpty()){
             if(iCRUDImpl.getUser(usernameTextId.getText()) != null){
                 if(user.getPassword().equals(passwordTextId.getText())){
@@ -30,22 +36,28 @@ public class Login {
                     switch (option){
                         case "Doctor":
                             System.out.println("Doctor");
+                            openScene("doctor_main_screen.fxml");
                             break;
                         case "Lab_Agent":
                             System.out.println("Lab Agent");
+                            openScene(".fxml");
                             break;
                         case "Clinic_Agent":
                             System.out.println("Clinic Agent");
+                            openScene("clinic_change.fxml");
                             break;
                         case "Transfer_Office_Agent":
                             System.out.println("Transfer Office Agent");
+                            openScene(".fxml");
                             break;
                         case "Admin":
                             System.out.println("Admin");
+                            openScene("admin_choose_hospital.fxml");
                             break;
                         default:
                             System.out.println("No match");
                     }
+                    closeButtonAction();
                 } else {
                     System.err.println("Wrong password!");
                     information.setTitle("Information");
@@ -57,7 +69,7 @@ public class Login {
                 System.err.println("There is no user with that username!");
                 warning.setTitle("Warning");
                 warning.setHeaderText("No user found");
-                warning.setContentText("No-one with that username is recorded on our database!");
+                warning.setContentText("No-one with that username is recorded in our database!");
                 warning.showAndWait();
             }
         } else {
@@ -67,5 +79,22 @@ public class Login {
             error.setContentText("Please fill all fields!");
             error.showAndWait();
         }
+    }
+
+    private void openScene(String fxmlFile) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(fxmlFile));
+        Scene scene = new Scene(parent);
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("I-aso");
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void closeButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) loginButtonId.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }
