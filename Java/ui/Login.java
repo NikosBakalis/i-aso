29caa6a353c988dd8808f88bc6548b9885fa6740
NikosBakalis <bakalis.nikolas@gmail.com>
2020-05-09 20:08:41 +0300

    Close window action added!

--- Java/ui/Login.java
+++ Java/ui/Login.java
@@ -92,9 +92,7 @@ public class Login {
     }
 
     private void closeButtonAction(){
-        // get a handle to the stage
         Stage stage = (Stage) loginButtonId.getScene().getWindow();
-        // do what you have to do
         stage.close();
     }
 }
