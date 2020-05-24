86f2880deea2ca1421256c7095d170f79bda0fd6
Panagiotis <panagiotis.tsakas2@gmail.com>
2020-05-24 20:58:03 +0300

    possible matches screen added tableview and scrollpane probably need coding

---
+++ Java/ui/doctor/PossibleMatchesScreen.java
@@ -0,0 +1,15 @@
+package ui.doctor;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class PossibleMatchesScreen {
+    public Button returnButton;
+    public Button confirmationButton;
+
+    public void onReturnClick(ActionEvent actionEvent) {
+    }
+
+    public void onConfirmationClick(ActionEvent actionEvent) {
+    }
+}
