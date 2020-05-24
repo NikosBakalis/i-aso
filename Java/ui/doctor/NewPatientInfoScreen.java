5d3730b0740914da864063eb8d9d07254d4babcd
Panagiotis <panagiotis.tsakas2@gmail.com>
2020-05-24 18:23:43 +0300

    new patient info screen added check attempted styling religion must be deleted

---
+++ Java/ui/doctor/NewPatientInfoScreen.java
@@ -0,0 +1,15 @@
+package ui.doctor;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class NewPatientInfoScreen {
+    public Button searchButton;
+    public Button returnButton;
+
+    public void onSearchClick(ActionEvent actionEvent) {
+    }
+
+    public void onReturnClick(ActionEvent actionEvent) {
+    }
+}
