c3a29d0dba696946c25d5bc1f7b7597f511d35c0
Panagiotis <panagiotis.tsakas2@gmail.com>
2020-05-24 21:24:13 +0300

    patient file screen added scrollpane probably needs coding

---
+++ Java/ui/doctor/PatientFileScreen.java
@@ -0,0 +1,47 @@
+package ui.doctor;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class PatientFileScreen {
+    public Button returnButton;
+    public Button patientInfoButton;
+    public Button briefHistoryButton;
+    public Button detailedHistoryButton;
+    public Button admissionTicketButton;
+    public Button statusAndDiagnosisButton;
+    public Button testsButton;
+    public Button testResultsButton;
+    public Button treatmentButton;
+    public Button dischargeNoteButton;
+
+    public void onReturnClick(ActionEvent actionEvent) {
+    }
+
+    public void onPatientInfoClick(ActionEvent actionEvent) {
+    }
+
+    public void onBriefHistoryClick(ActionEvent actionEvent) {
+    }
+
+    public void onDetailedHistoryClick(ActionEvent actionEvent) {
+    }
+
+    public void onAdmissionTicketClick(ActionEvent actionEvent) {
+    }
+
+    public void onStatusAndDiagnosisClick(ActionEvent actionEvent) {
+    }
+
+    public void onTestsClick(ActionEvent actionEvent) {
+    }
+
+    public void onTestResultsClick(ActionEvent actionEvent) {
+    }
+
+    public void onTreatmentClick(ActionEvent actionEvent) {
+    }
+
+    public void onDischargeNoteClick(ActionEvent actionEvent) {
+    }
+}
