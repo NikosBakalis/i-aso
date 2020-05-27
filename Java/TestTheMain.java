a230c584984fa0bcca97376db0f80d1c727b0274
Αννα <annamayaki@gmail.com>
2020-05-27 12:33:39 +0300

    patient file screen in now ready to action.

--- Java/TestTheMain.java
+++ Java/TestTheMain.java
@@ -15,7 +15,7 @@ public class TestTheMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("ui/transfer_office/discharge_note_and_billing_screen.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("ui/doctor/patient_file_screen.fxml"));
         Scene scene = new Scene(parent);
         primaryStage.setTitle("I-aso");
         scene.getStylesheets().add(getClass().getResource("ui/application.css").toExternalForm());
--- Java/ui/doctor/PatientFileScreen.java
+++ Java/ui/doctor/PatientFileScreen.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +24,27 @@ public class PatientFileScreen {
     public Button testResultsButton;
     public Button treatmentButton;
     public Button dischargeNoteButton;
+    public ScrollPane initialPane;
+    public Pane firstPane;
+    public Pane statusAndDiagnosisPane;
+    public ScrollPane infoDataPatient; // inside statusAndDiagnosisPane
+    public TextArea doctorDiagnosisArea; // inside statusAndDiagnosisPane
+    public Button saveStatusAndDiagnosisButton; // inside statusAndDiagnosisPane
+    public Pane treatmentPane;
+    public ScrollPane treatmentDataPatient;  //inside treatmentPane
+    public TextArea doctorTreatmentArea;  //inside treatmentPane
+    public Button saveTreatmentButton;   //inside treatmentPane
+    public Pane dischargeNotePane;
+    public TextArea dischargeText; //inside dischargeNotePane
+    public ComboBox hostClinic;   //inside dischargeNotePane
+    public Button dischargeNoteSaveButton;  //inside dischargeNotePane
+
+    // first pane is the pane that contains all the buttons etc.. This pain never goes.
+    // we use initial pane as the pane that only takes data from db. in this pane doctor is not supposed to make any changes.
+    // so in order not to have many panes in "PatientInfo" , "BriefHistory", we use initial pane.
+    // status and diagnosis pane only refers to status and diagnosis
+    //treatment pne is the pane that refers to treatment
+    // dischargeNotePane is a pane that refers to discharge note
 
     public void onReturnClick(ActionEvent actionEvent) throws IOException {
         System.out.println("Return to initial doctor screen");
@@ -29,37 +54,73 @@ public class PatientFileScreen {
 
     public void onPatientInfoClick(ActionEvent actionEvent) {
         System.out.println("Show patient info");
+        initialPane.setVisible(true);
+        statusAndDiagnosisPane.setVisible(false);
+        treatmentPane.setVisible(false);
+        dischargeNotePane.setVisible(false);
     }
 
     public void onBriefHistoryClick(ActionEvent actionEvent) {
+        initialPane.setVisible(true);
+        statusAndDiagnosisPane.setVisible(false);
+        treatmentPane.setVisible(false);
+        dischargeNotePane.setVisible(false);
         System.out.println("Show brief history");
     }
-
+   // false because we are not supposed to do this.
     public void onDetailedHistoryClick(ActionEvent actionEvent) {
+        initialPane.setVisible(false);
+        statusAndDiagnosisPane.setVisible(false);
+        treatmentPane.setVisible(false);
+        dischargeNotePane.setVisible(false);
         System.out.println("Show detailed history");
     }
-
+// in this phase doctor can only see the admission ticket. Not change it
     public void onAdmissionTicketClick(ActionEvent actionEvent) {
+        initialPane.setVisible(true);
+        statusAndDiagnosisPane.setVisible(false);
+        treatmentPane.setVisible(false);
+        dischargeNotePane.setVisible(false);
         System.out.println("Show admission ticket");
     }
 
     public void onStatusAndDiagnosisClick(ActionEvent actionEvent) {
+        initialPane.setVisible(false);
+        statusAndDiagnosisPane.setVisible(true);
+        treatmentPane.setVisible(false);
+        dischargeNotePane.setVisible(false);
         System.out.println("Show status and diagnosis");
     }
-
+    // false because we are not supposed to do this.
     public void onTestsClick(ActionEvent actionEvent) {
+        initialPane.setVisible(false);
+        treatmentPane.setVisible(false);
+        statusAndDiagnosisPane.setVisible(false);
+        dischargeNotePane.setVisible(false);
         System.out.println("Show lab tests");
     }
-
+    // false because we are not supposed to do this.
     public void onTestResultsClick(ActionEvent actionEvent) {
+        initialPane.setVisible(false);
+        treatmentPane.setVisible(false);
+        dischargeNotePane.setVisible(false);
+        statusAndDiagnosisPane.setVisible(false);
         System.out.println("Show lab tests results");
     }
 
     public void onTreatmentClick(ActionEvent actionEvent) {
+        initialPane.setVisible(false);
+        statusAndDiagnosisPane.setVisible(false);
+        treatmentPane.setVisible(true);
+        dischargeNotePane.setVisible(false);
         System.out.println("Show treatment");
     }
 
     public void onDischargeNoteClick(ActionEvent actionEvent) {
+        initialPane.setVisible(false);
+        statusAndDiagnosisPane.setVisible(false);
+        treatmentPane.setVisible(false);
+        dischargeNotePane.setVisible(true);
         System.out.println("Show discharge note");
     }
 
@@ -76,5 +137,22 @@ public class PatientFileScreen {
     private void closeButtonAction(){
         Stage stage = (Stage) returnButton.getScene().getWindow();
         stage.close();
+    }
+
+    // inside statusAndDiagnosisPane
+    public void onSaveStatusAndDignosisClick(ActionEvent actionEvent) {
+    }
+    //inside treatmentPane
+    public void onSaveTreatmentClick(ActionEvent actionEvent) {
+    }
+
+    public void onHostClinicClick(ActionEvent actionEvent) throws IOException {
+        openScene("initial_doctor_screen.fxml");
+        closeButtonAction();
+
+    }
+    public void onDischargeNoteSaveClick(ActionEvent actionEvent) throws IOException {
+        openScene("initial_doctor_screen.fxml");
+        closeButtonAction();
     }
 }
